79c17b0a5814e52ce63d59b65271eab9bfc89f4f
NicolD PC <nicolgit@users.noreply.github.com>
2020-09-22 23:36:23 +0200

    hello world commit :-)

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace core_sensor_reader
+{
+    class Program
+    {
+        static string port = "/dev/ttyUSB0";
+
+        static SerialPort _serialPort; 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nove SDS011 PM reader - NicolD");
+
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();            
+            Console.WriteLine("The following serial ports were found:");       
+
+            // Display each port name to the console.             
+            foreach(string port in ports)             
+            {                 
+                Console.WriteLine(port);             
+            }             
+
+            _serialPort = new SerialPort("/dev/ttyUSB0");
+            
+            // Set the read/write timeouts             
+            _serialPort.ReadTimeout = 1500;             
+            _serialPort.WriteTimeout = 1500;
+            _serialPort.Open();
+            Console.WriteLine($"Port {port} Opened successfully.");     
+
+            int i = 0;
+            while (i<1000)
+            {
+                var b = _serialPort.ReadByte();
+                Console.Write (b.ToString());
+
+                i++;
+            }
+
+            _serialPort.Close();            
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+.NET CORE app that reads from Nova PM Sensor SDS011 High Precision Laser PM2.5 Air Quality Detection Sensor
+
+**publish for Raspberry**
+
+dotnet publish -o publish --self-contained -r linux-arm
+
+**copy to raspberry ALL files**
+
+scp .\publish\* pi@raspberry.fritz.box:/home/pi/projects/core-sensor-reader/
+
+**copy to raspberry only 'core-*' files**
+
+scp .\publish\core-* pi@raspberry.fritz.box:/home/pi/projects/core-sensor-reader/
