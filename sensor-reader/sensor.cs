81a9a75df653c5a5a6fb9dba68be1272bfc097ff
NicolGit <nicola.delfino@outlook.com>
2020-11-11 22:49:11 +0100

    more logs

--- sensor-reader/sensor.cs
+++ sensor-reader/sensor.cs
@@ -151,6 +151,7 @@ public class sensor
         {
             Console.Write(Convert.ToString(b, 16).PadLeft(2, '0'));
         }
-        Console.WriteLine($"  PM2.5 {DateTime.Now.ToString("yyyyMMdd HHmmss")} {PM25.ToString("00.0")} μg/m3 - PM10 {PM10.ToString("00.0")} μg/m3");
+        Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd HHmmss")} PM2.5 {PM25.ToString("00.0")} μg/m3 - PM10 {PM10.ToString("00.0")} μg/m3");
+        Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd HHmmss")} Temperature {Temperature.ToString()} - Pressure {Pressure.ToString()} - Humidity {Humidity.ToString()}%");
     }
 }
