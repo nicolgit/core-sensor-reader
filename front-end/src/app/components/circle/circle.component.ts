4684d98aa0b32c7d0b465ae8804219eb56d40a45
Francesco Mannarino <francesco.mannarino@eng.it>
2021-03-03 11:45:35 +0100

    inserito controllo che adegua il colore dei font in base alla presenza della dark mode

--- front-end/src/app/components/circle/circle.component.ts
+++ front-end/src/app/components/circle/circle.component.ts
@@ -1,3 +1,4 @@
+import { CloneVisitor } from "@angular/compiler/src/i18n/i18n_ast";
 import { Component, Input, OnInit } from "@angular/core";
 import { Street } from "src/app/models/street.model";
 
@@ -9,9 +10,9 @@ import { Street } from "src/app/models/street.model";
 export class CircleComponent implements OnInit {
   @Input("pollutionAreaData") pollutionAreaData: Street;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   calculatePollutionColor(value, type): string {
     let color: string;
@@ -25,17 +26,23 @@ export class CircleComponent implements OnInit {
         value >= 0 && value <= 10
           ? "#78C000"
           : value > 10 && value <= 25
-          ? "#e8f533"
-          : "#e31b25";
+            ? "#e8f533"
+            : "#e31b25";
     } else {
       color =
         value >= 0 && value <= 20
           ? "#78C000"
           : value > 20 && value <= 50
-          ? "#e8f533"
-          : "#e31b25";
+            ? "#e8f533"
+            : "#e31b25";
     }
 
     return color;
   }
+
+  calculateContentColor(): string {
+    // controllo se è attiva la dark mode
+    return (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ? '#ffffff' : '#000000';
+  }
+
 }
